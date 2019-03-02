4672225c56f358101ef5740d445b012d7f335962
alexodonn <alexodonn@gmail.com>
2019-03-02 14:22:26 -0500

    pathfinding

--- utils.py
+++ utils.py
@@ -95,3 +95,60 @@ def move_bot(bkend: IBackend, current_location: Tuple[int, int], target_location
 
 def dump_garbage(bkend: IBackend, inst: instance.Instance):
     pass
+
+'''
+    Clip coordinate between 0 and a set maximum. return a tuple containing the coord
+'''
+def clip_coord(x, y, max_x, max_y):
+
+    x = max(0, min(x, max_x-1))
+    y = max(0, min(y, max_y-1))
+    return (x, y)
+    
+
+'''
+    Get ideal scan path coordinates
+'''
+def get_scan_path(size_x: int, size_y: int, scan_w):
+    path = []
+
+    curr_x = 0
+    curr_y = 0
+
+    # always start at top
+    path.append((curr_x, curr_y))
+
+    half_w = math.ceil(scan_w/2)
+
+    stride_x = scan_w + 1
+
+    stride_y = -scan_w
+
+    # from top left until we are completely out of bounds 
+    # (last scan would ideally be the bottom right corner)
+    while True:
+
+        # if we're out of bounds already, go down our scan width +1 and our scan width over to the right.
+        if curr_x >= size_x or curr_x < 0 \
+            or curr_y >= size_y or curr_y < 0:
+
+            if curr_x >= (size_x - scan_w) and (curr_y >= size_y - scan_w):
+                break
+
+            curr_y += scan_w + 1
+            curr_x += scan_w
+
+            # add the start of the next spiral to our path
+            path.append(clip_coord(curr_x, curr_y, size_x, size_y))
+
+            # reverse x and y direction.
+            stride_x *= -1
+            stride_y *= -1
+
+        # stride no matter what
+        curr_x += stride_x
+        curr_y += stride_y
+
+        path.append(clip_coord(curr_x, curr_y, size_x, size_y))
+
+    return path
