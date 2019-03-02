b14c7e71701e4a627cf0122a154a04cf0374d3f4
Brandon Richardson <brandon1024.br@gmail.com>
2019-03-02 09:37:46 -0500

    added api token

--- backend.py
+++ backend.py
@@ -1,9 +1,10 @@
 import requests
 
-BASE = 'https://someurl.com'
+BASE = 'http://cec2019.ca'
+API_TOKEN = 'brunswick-8Nq9JsYFUtFzdReih3P8s2YMQiRQHDRMkWNkASN5A8xMGa4yzq5njUv4hFEEaBbZ'
 
 def get(url):
-    return requests.get(BASE + url)
+    return requests.get(BASE + url, headers={'token': API_TOKEN})
 
 def some_endpoint():
     return get('/some_endpoint')
