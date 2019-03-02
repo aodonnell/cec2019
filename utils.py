0400291e6361c7d27e1a1d64bc36b73f12728b92
Amir Eldesoky <aeldesok@unb.ca>
2019-03-02 12:17:40 -0500

    adding the path transformation

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import logging
+import instance
 
 FORMAT = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
 
@@ -11,3 +12,18 @@ def get_logger(name: str, level=logging.INFO):
     logger.addHandler(handler)
 
     return logger
+
+def transform_path(instance, starting_x, starting_y, path: list):
+
+    if(starting_x == instance.x_min and starting_y == instance.x_min):
+        transformed_path = path
+    elif(starting_x == instance.x_max and starting_y == instance.y_max):
+         transformed_path = path.reverse()
+    elif(starting_x == instance.x_max and starting_y == instance.y_min):
+         transformed_path = [(x,-y) for x,y in path]
+    else:
+         transformed_path = [(-x,y) for x,y in path]
+    
+    translated_path = [(x + instance.x_min, y + instance.y_min) for (x,y) in transformed_path]
+
+    return translated_path
